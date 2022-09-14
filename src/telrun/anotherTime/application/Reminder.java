ce06c863fd70eaa3bb8ca993c742254eba456670
volnykot <volnykot3@gmail.com>
2022-09-14 04:15:21 +0300

    adds HW#26 optimized

--- src/telrun/anotherTime/application/Reminder.java
+++ src/telrun/anotherTime/application/Reminder.java
@@ -7,28 +7,27 @@ public class Reminder {
 
 	public static void main(String[] args) throws Exception {
 		try {
-			int[] inputValues = adjustInpurValues(args);
-			ring(inputValues[0], inputValues[1]);
+			int[] stepAndDuration = getStepAndDuration(args);
+			ring(stepAndDuration[0], stepAndDuration[1]);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	private static int[] adjustInpurValues(String[] args) throws Exception {
+	private static int[] getStepAndDuration(String[] args) throws Exception {
 		int[] res = new int[2];
 		ChronoUnit unit = getUnit(args[1]);
 		res[0] = convertToMillis(Integer.parseInt(args[0]), unit);
-		res[1] = args.length == 3 ? convertToMillis(Integer.parseInt(args[2]), unit)
+		res[1] = args.length > 2 ? convertToMillis(Integer.parseInt(args[2]), unit)
 				: convertToMillis(1, ChronoUnit.HOURS);
 		return res;
 	}
 
-	private static void ring(int stepInMillis, int duration) throws InterruptedException {
-		int counter = duration / stepInMillis;
-		while (counter != 0) {
-			Thread.sleep(stepInMillis);
-			System.out.println("\007");
-			counter--;
+	private static void ring(int step, int duration) throws InterruptedException {
+		int counter = duration / step;
+		while (counter-- != 0) {
+			Thread.sleep(step);
+			System.out.println("\007\007\007");
 		}
 	}
 
@@ -38,9 +37,9 @@ public class Reminder {
 
 	private static ChronoUnit getUnit(String inputValue) throws Exception {
 		var res = inputValue.toUpperCase();
-		boolean flIsMatch = Arrays.stream(ChronoUnit.values())
+		boolean isInputValueCorrect = Arrays.stream(ChronoUnit.values())
 				.anyMatch(unit -> res.equals(unit.toString().toUpperCase()));
-		if (!flIsMatch) {
+		if (!isInputValueCorrect) {
 			throw new Exception(String.format(" '%s' is incorrect. Plz enter correct value", inputValue));
 		}
 		return ChronoUnit.valueOf(res);
